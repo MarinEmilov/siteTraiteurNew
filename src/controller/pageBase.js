bd64e70d413ecd28f828700b51564fb5456ec112
MarinEmilov <2175900@cstjean.qc.ca>
2021-11-22 22:50:40 -0500

    avec route de pages

--- src/controller/pageBase.js
+++ src/controller/pageBase.js
@@ -11,4 +11,34 @@ exports.getLoginPage = ( req, res , next) => {
     res.render("index", {});
 }
 
+exports.getVgPage = ( req, res , next) => {
+    res.render("vg", {});
+}
 
+exports.getSansGlutenPage = ( req, res , next) => {
+    res.render("sansgluten", {});
+}
+
+exports.getFamilialePage = ( req, res , next) => {
+    res.render("familiale", {});
+}
+
+exports.getDejeunerPage = ( req, res , next) => {
+    res.render("dejeuner", {});
+}
+
+exports.getIndividuellePage = ( req, res , next) => {
+    res.render("individuelle", {});
+}
+
+exports.getEmploiPage = ( req, res , next) => {
+    res.render("emploi", {});
+}
+
+exports.getHistoirePage = ( req, res , next) => {
+    res.render("histoire", {});
+}
+
+exports.getLocationPage = ( req, res , next) => {
+    res.render("location", {});
+}
--- src/routes/pageBase.js
+++ src/routes/pageBase.js
@@ -4,5 +4,13 @@ let pageBase = require('../controller/pageBase.js');
 
 router.get('/', pageBase.getHomepage);
 router.get('/index', pageBase.getLoginPage);
+router.get('/vg', pageBase.getVgPage);
+router.get('/sansgluten', pageBase.getSansGlutenPage);
+router.get('/familiale', pageBase.getFamilialePage);
+router.get('/dejeuner', pageBase.getDejeunerPage);
+router.get('/individuelle', pageBase.getIndividuellePage);
+router.get('/emploi', pageBase.getEmploiPage);
+router.get('/histoire', pageBase.getHistoirePage);
+router.get('/location', pageBase.getLocationPage);
 
 module.exports = router;
